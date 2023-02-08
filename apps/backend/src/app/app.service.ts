c9ba0dc6fe5d18ac6afe1a5e86cdbec92c3f528d
Felix Hilgers <felix_hilgers@posteo.de>
2023-02-08 19:37:42 +0100

    feat(database): add prisma

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaClient } from '@cineman/database';
+
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to cineman-backend!' };
+
+  client: PrismaClient = new PrismaClient();
+
+  constructor() {
+      this.client.$connect
   }
+
+  async createData() {
+    return this.client.todo.create({ data: {} });
+  }
+
+  async getData() {
+    return this.client.todo.findMany({});
+  }
+
 }
---
+++ libs/database/prisma/migrations/20230208183509_dev/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "todos" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "todos_pkey" PRIMARY KEY ("id")
+);
