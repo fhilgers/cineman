49ecfa6a3aeb07160b295dfa772c9302e2c2a0f3
Felix Hilgers <felix_hilgers@posteo.de>
2023-02-08 16:52:29 +0100

    fix(backend): remove global prefix

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,13 +10,16 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = '';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+  Logger.log(
+    `Database is running on: ${process.env.DATABASE_URL}`
+  )
 }
 
 bootstrap();
