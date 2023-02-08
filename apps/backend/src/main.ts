335e2d8960e4dffd6aa2d39f3934d0a062ad0193
Felix Hilgers <felix_hilgers@posteo.de>
2023-02-09 00:37:50 +0100

    feat(backend): validate and transform

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,16 +3,23 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = '';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe({whitelist: true, enableDebugMessages: true, transform: true}));
+  app.enableCors();
   const port = process.env.PORT || 3333;
+  
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
